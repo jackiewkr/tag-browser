b456e6f61aff35837f9fe442a2a2a0799ca95f9e
Jacqueline <jackiewalkergeneral@gmail.com>
2025-04-10 21:31:35 +0100

    Searching & context menu development
    Searchbar allows for tag searching, evaluated from left to right.
    Context menu has been added to allow selection of a item, with the
    ability to modify said item via a dialog.
    
    Next, delete menu. After that, the program should be functionally
    complete.

--- tag-browser.py
+++ tag-browser.py
@@ -19,21 +19,24 @@ import os
 
 class TaggedItem():
     """Item in database. Stores tags related to file."""
-    def __init__(self, filename: str, dispname: str="", tags: list[str]=[]):
+    def __init__(self, uid: int, filename: str, dispname: str="",
+                 tags: list[str]=[]):
+        self.uid = uid
         self.__filename = ""
         self.__dispname = dispname
         self.__tags = tags
 
         self.set_filename(filename)
 
-    def add_tag(self, tag: str):
-        self.__tags.append(tag)
+    def set_tags(self, tags: list[str]):
+        self.__tags = []
+        for tag in tags:
+            self.__tags.append(tag.strip())
 
-    def remove_tag(self, tag: str):
-        if tag in self.__tags:
-            self.__tags.remove(tag)
-
-    def is_tagged(self, tag: int) -> bool:
+    def is_tagged(self, tag: str) -> bool:
+        print(tag + ": ->" + ",".join(self.__tags))
+        for i in self.__tags:
+            print("'"+i.strip()+"'")
         if tag in self.__tags:
             return True
         return False
@@ -46,7 +49,7 @@ class TaggedItem():
             raise FileNotFoundError
 
     def set_dispname(self, dname: str):
-        self.__dispname = dispname
+        self.__dispname = dname
 
     def get_filename(self) -> str:
         return self.__filename
@@ -93,6 +96,47 @@ class AddDialog(simpledialog.Dialog):
 
         return master
 
+class ModDialog(simpledialog.Dialog):
+    """Dialog for modifying an item into the database.
+    TODO: merge into one dialog?"""
+    def __init__(self, parent, ti): 
+        self.filename = ti.get_filename()
+        self.dispname = StringVar()
+        self.tags = StringVar()
+
+        self.dispname.set(ti.get_dispname())
+        self.tags.set(", ".join(ti.get_tags()))
+        super().__init__(parent)
+
+    def __cbk_filedlg(self):
+        """Callback for opening file dialog"""
+        self.filename = filedialog.askopenfilename()
+        self.fbtn["text"] = self.filename
+
+    def body(self, master):
+        # mainframe for dlg
+        mf = ttk.Frame(master)
+        mf.grid(row=0,column=0,sticky=(N,E,W,S))
+        
+        # Choose file button opens file dlg
+        ttk.Label(mf, text="Choose File:").grid(row=1,column=0,sticky=(E))
+        self.fbtn = ttk.Button(mf, text=self.filename, 
+                               command=self.__cbk_filedlg)
+        self.fbtn.grid(row=1,column=1,sticky=(N,E,W,S))
+
+        # Entry for writing display name
+        ttk.Label(mf, text="Display Name:").grid(row=2,column=0,sticky=(E))
+        ttk.Entry(mf,textvariable=self.dispname).grid(row=2,column=1,
+                                                      sticky=(N,E,W,S))
+        
+        # Entry for writing tags (csv)
+        ttk.Label(mf, text="Tags (CSV):").grid(row=3,column=0,sticky=(E))
+        ttk.Entry(mf,textvariable=self.tags).grid(row=3,column=1,
+                                                  sticky=(N,E,W,S))
+
+        return master
+
+
 
 class Window(Tk):
     """Main Tkinter window. Handles program logic & displaying."""
@@ -104,6 +148,8 @@ class Window(Tk):
 
         # Store items in "database"
         self.__items = []
+        self.__curr_items = []
+        self.__cntr = 0;
 
         # Mainframe for displaying stuff
         self.frame = ttk.Frame(self)
@@ -122,6 +168,7 @@ class Window(Tk):
         self.searchval = StringVar()
         self.searchbar = ttk.Entry(self.frame, textvariable=self.searchval)
         self.searchbar.grid(column=0, row=1, sticky=(N,E,W,S))
+        self.searchbar.bind("<Return>", self.__cbk_search)
         self.frame.columnconfigure(0, weight=1)
 
         # Search results frame
@@ -138,8 +185,12 @@ class Window(Tk):
         self.resultsframe.columnconfigure(0, weight=1)
         self.resultsframe.rowconfigure(0, weight=1)
 
+        # Context menu for popup on treeview
+        self.contextmenu = Menu(self.tree)
+        self.tree.bind("<Button-3>", self.__cbk_context)
 
-        self.load()
+
+        self.load() 
 
     def save(self):
         """Save items to file. Each line is an item in the dict, value
@@ -158,18 +209,21 @@ class Window(Tk):
         self.__items = []
 
         fptr = open("dict.csv", "r")
+        self.__cntr = 0;
         for i in fptr:
             values = i.strip("\n").split("|")
-            print(values)
+            tags = []
+            for i in values[2].split(","):
+                tags.append(i.strip())
             try:
-                ti = TaggedItem(values[0], dispname=values[1],
-                                           tags=values[2].split(","))
+                ti = TaggedItem(self.__cntr, values[0], dispname=values[1], tags=tags)
                 self.__items.append(ti)
+                self.__cntr += 1;
             except FileNotFoundError:
                 messagebox.showerror(message=values[1] + " is not a valid file!\
                                      Item has been removed from dictionary.")
             
-
+        self.__curr_items = self.__items
         self.tree_redraw()
 
 
@@ -189,9 +243,10 @@ class Window(Tk):
                 # Format tags from dialog into proper format
                 tags = "".join(dlg.tags.get().strip()).split(",")
                 print(tags)
-                ti = TaggedItem(dlg.filename, dispname=dlg.dispname.get(),
-                                tags=tags)
+                ti = TaggedItem(self.__cntr, dlg.filename,
+                                dispname=dlg.dispname.get(), tags=tags)
                 correct_format = True
+                self.__cntr += 1
             except FileNotFoundError:
                 messagebox.showerror(message="File specified is not valid. \
                                                 Please choose another.")
@@ -200,16 +255,81 @@ class Window(Tk):
         self.__items.append(ti)
         self.tree_redraw()
 
+    def __cbk_search(self, event):
+        """Callback for searching a tag
+        Tags are evaluated left to right
+
+        TAG: search for tag
+        !TAG: search for anything without tag
+        TAG ATAG BTAG: seacrh for anything with all three tags"""
+        criteria = self.searchval.get().split(" ")
+        self.__curr_items = self.__items
+        # early exit for empty searchbar
+        if criteria[0] == "":
+            self.tree_redraw()
+            return
+
+        for tok in criteria:
+            temp_curr_items = []
+
+            if tok[0] == "!":
+                for item in self.__curr_items:
+                    if item.is_tagged(tok[1:]) == False:
+                        temp_curr_items.append(item)
+            else:
+                for item in self.__curr_items:
+                    if item.is_tagged(tok):
+                        temp_curr_items.append(item)
+
+            self.__curr_items = temp_curr_items
+
+        self.tree_redraw()
+
+    def __cbk_modify(self, uid):
+        """Callback for pressing "Modify item" menu button."""
+        correct_format = False
+        while not(correct_format):
+            try:
+                dlg = ModDialog(self, self.__items[uid])
+                correct_format = True
+            except FileNotFoundError:
+                messagebox.showerror(message="File specified is not valid. \
+                                                Please choose another.")
+        tags = "".join(dlg.tags.get().strip()).split(",")
+
+        # Add only if correct format
+        self.__items[uid].set_filename(dlg.filename)
+        self.__items[uid].set_dispname(dlg.dispname.get())
+        self.__items[uid].set_tags(tags)
+
+        self.tree_redraw()
+
+
+    def __cbk_context(self, event):
+        """Repopulate context menu with correct items relating to selected
+        item. Handle other context menu tasks."""
+        self.contextmenu.delete(0, "end")
+        uid = int(self.tree.identify_row(event.y))
+        self.tree.selection_set(uid)
+
+        self.contextmenu.add_command(label="Modify item",
+                                     command=lambda u=uid: self.__cbk_modify(u))
+        self.contextmenu.add_command(label="Delete item",
+                                     command=lambda u=uid: self.__cbk_delete(u))
+
+        self.contextmenu.tk_popup(event.x_root, event.y_root)
+
+
     def tree_redraw(self):
         """Redraw treeview from scratch. Call when
         adding/removing/editing TaggedItem."""
         self.tree.delete(*self.tree.get_children())
 
-        for item in self.__items:
+        for item in self.__curr_items:
             tag_str = ", ".join(item.get_tags())
             self.tree.insert("", END, text=item.get_dispname(),
                              values=(item.get_filename(),
-                                     " ".join(tag_str.split())))
+                                     " ".join(tag_str.split())), iid=item.uid)
 
 
 win = Window()
